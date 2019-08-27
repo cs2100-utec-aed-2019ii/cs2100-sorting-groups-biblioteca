303dfe4228abdbd8f53e48d93e2204d38944975f
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2019-08-27 18:10:17 -0500

    feat(InsertSort)
    Añadida la funcion InserSort, pero incompleta

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+int InsertSort(int *array, int size){
+    for (int i = 1; i < size; i++)
+    {
+        if (array[i] < array[i-1])
+        {
+            int aux = array[i];
+            for (int j = i-1; j >= 0; j--){
+                if(aux < array[j]){
+                    for (int k = 0; k < i-j; k++)
+                    {
+                        array[i-k] = array[j-k]
+                    }
+                    
+                }
+            }
+        }      
+    }
+}
+
 int main (int, char * []){
     std::cout << "Hello World" << std::endl;
     return 1;
