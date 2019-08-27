cf41793031f89b1fbd053179e8a1e1e1e4d593a9
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2019-08-27 13:40:02 -0500

    feat(SelectionSort)
    avance del selection sort

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+int SelectionSort(int *array){
+    int min = array[0];
+    int SortedArray[sizeof(array)];
+    for (int i=0; i < sizeof(array);i++){
+        if (array[i]<min)
+        {
+            min = array[i];
+        }
+        for (int j = 0; j < sizeof(array); i++)
+        {
+            SortedArray[j] = min;
+        }
+        
+        
+    }
+    return array;
+}
+
+
 int main (int, char * []){
     std::cout << "Hello World" << std::endl;
     return 1;
