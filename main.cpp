3d568d17eecb2c23263cd33c0c4e8042019e20b1
marlon <marlon.mejia@utec.edu.pe>
2019-08-28 16:22:58 -0500

    feat(Bubblesort)
    Funcion Bubblesort

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
+int Bubble_sort(int var_array[], int var_size){
+    int var_last = var_size-1;
+    for (int i=0; i<var_last;i++){
+        for(int j=var_size-1; j>i;j--){
+            if(var_array[j-1]>var_array[j]) {
+                int var_temp;
+                var_temp= var_array[j-1];
+                var_array[j-1]=var_array[j];
+                var_array[j]= var_temp;
+            }
+        }
+    }
+    return var_array[var_size];
+}
+
+
 int main (int, char * []){
     std::cout << "Hello World" << std::endl;
     return 1;
