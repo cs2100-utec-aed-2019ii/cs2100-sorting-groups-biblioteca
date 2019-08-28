a0128aa70fdfda4f132aeec38fdd9ee3387f7c37
marlon <marlon.mejia@utec.edu.pe>
2019-08-28 17:30:13 -0500

    feat(Bubblesort): Integracion de la funcion con la clase

--- main.cpp
+++ main.cpp
@@ -1,22 +1,142 @@
 #include <iostream>
+#include <time.h>
+using namespace std;
 
-int Bubble_sort(int var_array[], int var_size){
-    int var_last = var_size-1;
-    for (int i=0; i<var_last;i++){
-        for(int j=var_size-1; j>i;j--){
-            if(var_array[j-1]>var_array[j]) {
-                int var_temp;
-                var_temp= var_array[j-1];
-                var_array[j-1]=var_array[j];
-                var_array[j]= var_temp;
+template <typename T>
+class Sorting {
+private:
+    T* lista = nullptr;
+    int tamano;
+public:
+    Sorting(int tamano): tamano(tamano){
+        lista = new T(tamano);
+    }
+    ~Sorting(){
+        delete lista;
+    }
+
+    T* get_lista(){ return lista; }
+
+    void Merge(int p, int q,int r)
+    {
+        int n1,n2,i,j,k;
+
+        n1=q-p+1;
+        n2=r-q;
+        T L[n1],R[n2];
+
+        for(i=0;i<n1;i++)
+        {
+            L[i]=lista[p+i];
+        }
+
+        for(j=0;j<n2;j++)
+        {
+            R[j]=lista[q+j+1];
+        }
+        i=0,j=0;
+
+        for(k=p;i<n1&&j<n2;k++)
+        {
+            if(L[i]<R[j])
+            {
+                lista[k]=L[i++];
+            }
+            else
+            {
+                lista[k]=R[j++];
             }
         }
+
+        while(i<n1)
+        {
+            lista[k++]=L[i++];
+        }
+
+        while(j<n2)
+        {
+            lista[k++]=R[j++];
+        }
     }
-    return var_array[var_size];
+
+
+    void MergeSort(int p,int r)
+    {
+        int q;
+        if(p<r)
+        {
+            q=(p+r)/2;
+            MergeSort(p,q);
+            MergeSort(q+1,r);
+            Merge(p,q,r);
+        }
+    }
+
+    void heapify(int n, int i)
+    {
+        int largest = i;
+        int l = 2*i + 1;
+        int r = 2*i + 2;
+
+        if (l < n && lista[l] > lista[largest])
+            largest = l;
+
+        if (r < n && lista[r] > lista[largest])
+            largest = r;
+
+        if (largest != i)
+        {
+            swap(lista[i], lista[largest]);
+
+            heapify(n, largest);
+        }
+    }
+
+    void heapSort()
+    {
+        for (int i = tamano / 2 - 1; i >= 0; i--)
+            heapify(tamano, i);
+
+        for (int i=tamano-1; i>=0; i--)
+        {
+            swap(lista[0], lista[i]);
+
+            heapify(i, 0);
+        }
+    }
+
+    void printArray()
+    {
+        for (int i=0; i < tamano; i++)
+            cout <<  lista[i] << ' ';
+        cout << endl;
+    }
+
+};
+
+void llenar_int(int* list, int max, int n){
+    srand(time(NULL));
+    for(int i = 0; i < n; i++){
+        list[i] = 1 + rand()%(max);
+    }
 }
 
+int main() {
 
-int main (int, char * []){
-    std::cout << "Hello World" << std::endl;
-    return 1;
+    cout << "Heap Sort: \n";
+    Sorting<int>* heap_sort = new Sorting<int>(10);
+    llenar_int(heap_sort->get_lista(),10,10);
+    heap_sort->printArray();
+    heap_sort->heapSort();
+    heap_sort->printArray();
+
+
+    cout << "Merge Sort: \n";
+    Sorting<int>* merge_sort = new Sorting<int>(16);
+    llenar_int(merge_sort->get_lista(),50,16);
+    merge_sort->printArray();
+    merge_sort->MergeSort(0,16);
+    merge_sort->printArray();
+
+    return 0;
 }
