478835a0994eac6da5105fa95cc13653be69dca2
Unknown <victor.ascuna@utec.edu.pe>
2019-08-28 15:59:22 -0500

    feat(HeapSort),fix(General)
    Implementación del HeapSort, correcciones del Main y el MergSort

--- main.cpp
+++ main.cpp
@@ -18,14 +18,14 @@ public:
         cout << endl;
     }
     
-    T Merge(int p, int q,int r)
+    void Merge(int p, int q,int r)
     {
 
         int n1,n2,i,j,k;
 
         n1=q-p+1;
         n2=r-q;
-        int L[n1],R[n2];
+        T L[n1],R[n2];
 
         for(i=0;i<n1;i++)
         {
@@ -61,7 +61,7 @@ public:
         }
     }
 
-    int MergeSort(int p,int r)
+    void MergeSort(int p,int r)
     {
         int q;
         if(p<r)
@@ -72,20 +72,64 @@ public:
             Merge(p,q,r);
         }
     }
+
+        void heapify(int n, int i)
+    {
+        int largest = i;
+        int l = 2*i + 1;
+        int r = 2*i + 2;
+
+        if (l < n && lista[l] > lista[largest])
+            largest = l;
+
+        if (r < n && lista[r] > lista[largest])
+            largest = r;
+
+        if (largest != i)
+        {
+            swap(lista[i], lista[largest]);
+
+            heapify(n, largest);
+        }
+    }
+
+    void heapSort()
+    {
+        for (int i = tamano / 2 - 1; i >= 0; i--)
+            heapify(tamano, i);
+
+        for (int i=tamano-1; i>=0; i--)
+        {
+            swap(lista[0], lista[i]);
+
+            heapify(i, 0);
+        }
+    }
 };
 
-void llenar_int(List<int>* lista,int max,int n){
+void llenar_int(int* list, int max, int n){
     srand(time(NULL));
     for(int i = 0; i < n; i++){
-        int data = rand()%(max+1);
-        lista->add_end(data);
+        list[i] = 1 + rand()%(max);
     }
 }
 
 int main (int, char * []){
-    Sorting<int>* lista = new Sorting<int>();
-    llenar_int(lista->get_lista(),50,20);
-    lista->MergeSort(0,20);
-    lista->printArray(20);
+        
+    cout << "Heap Sort: \n";
+    Sorting<int>* heap_sort = new Sorting<int>(16);
+    llenar_int(heap_sort->get_lista(),50,16);
+    heap_sort->printArray();
+    heap_sort->heapSort();
+    heap_sort->printArray();
+
+    
+    cout << "Merge Sort: \n";
+    Sorting<int>* merge_sort = new Sorting<int>(16);
+    llenar_int(merge_sort->get_lista(),50,16);
+    merge_sort->printArray();
+    merge_sort->MergeSort(0,16);
+    merge_sort->printArray();
+    
     return 1;
 }
