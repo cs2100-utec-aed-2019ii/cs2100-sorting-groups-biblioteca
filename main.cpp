785aad11ede64d334052b9123f516f0918ba43ca
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2019-08-28 11:53:38 -0500

    feat(InsertSort)
    completada la funcion

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 
 int InsertSort(int *array, int size){
-    for (int i = 1; i < size; i++)
-    {
-        if (array[i] < array[i-1])
-        {
-            int aux = array[i];
+    int contador = 0;
+    int aux = 0;
+    for (int i = 1; i < size; i++){
+        if (array[i] < array[i-1]){
+            aux = array[i];
             for (int j = i-1; j >= 0; j--){
                 if(aux < array[j]){
                     for (int k = 0; k < i-j; k++)
                     {
                         array[i-k] = array[j-k]
                     }
-                    
                 }
+                contador = j;
             }
+            array[contador] = aux;
+            contador = 0;
         }      
     }
 }
