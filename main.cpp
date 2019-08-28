7f5eb75d4365156c3a4e5cd1f6c347b8896bb212
Unknown <victor.ascuna@utec.edu.pe>
2019-08-28 13:53:41 -0500

    feat(general): Creacion de la lista de prueba y una funcion de llenado.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-template<typename T>
-class Sorting
-{
+#include <time.h>
+
+template <typename T>
+class Sorting {
 private:
     T* lista;
 public:
@@ -9,7 +10,15 @@ public:
     ~Sorting();
 };
 
-int main (){
-    
+void llenar_int(List<int>* lista,int max,int n){
+    srand(time(NULL));
+    for(int i = 0; i < n; i++){
+        int data = rand()%(max+1);
+        lista->add_end(data);
+    }
+}
 
+int main (int, char * []){
+    List<int>* lista = new List<int>();
+    return 1;
 }
